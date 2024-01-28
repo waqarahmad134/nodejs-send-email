8aa03b52d1b299ecb6073b1a55596bb78573f648
Waqar Ahmad <waqar4704649@gmail.com>
2024-01-28 14:07:39 +0500

    Nodejs send email function + Vercel deployment json

---
+++ README.md
@@ -0,0 +1,30 @@
+# Node.js send email tutorial.
+
+## Requirements
+
+* Nodejs 8.10.x
+
+Clone project and run test.
+
+```
+$ git clone https://git_url_clone <project_dir>
+$ cd <project_dir>
+$ npm install
+$ node index.js
+```
+
+* Change your admin email account in the file: **.env** if .env is not available then first create and use these Variables
+EMAIL_HOST
+EMAIL_USERNAME
+EMAIL_PASSWORD
+EMAIL_PORT
+
+"In this repository, I have also included a Vercel configuration. If you want to deploy your project on Vercel and run this API without any hosting costs, simply fork this repository and use your Vercel account. By selecting this repository in Vercel, your Node.js project will be live on Vercel, and you can manage the API from there. For example, if you forget to access the API at https://nodejs-send-email.vercel.co, your API endpoint will be https://nodejs-send-email.vercel.co/sendmail."
+
+Author: Waqar Ahmad
+
+Linkedin: https://www.linkedin.com/in/waqar134/
+
+"A little bit of fragrance always clings to the hands that gives you roses!"
+
+Thanks for watching!
---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const app = express();
+const PORT = 6786;
+
+app.use(cors());
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST || 'smtp.elasticemail.com',
+  port: process.env.EMAIL_PORT || '2525',
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USERNAME || 'adx-marketing@adxfactor.com',
+    pass: process.env.EMAIL_PASSWORD || 'FE6F4B89A3A33B0CAD868D1566F2BD308414',
+  },
+});
+
+
+app.post('/sendmail', (req, res) => {
+  const { name ,email , phone , subject ,message } = req.body;
+  const mailOptions = {
+    from: 'adx-marketing@adxfactor.com',
+    to: 'waqardevops134@gmail.com',
+    subject: `Query from ${name}`,
+    text: `Name: ${name} Email:${email} Phone:${phone} Subject:${subject} Message:${message}`
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      return res.json({ status: '0', message: 'Internal server error. Please try again' });
+    } else {
+      return res.json({ status: '1', message: 'Message sent successfully' });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
